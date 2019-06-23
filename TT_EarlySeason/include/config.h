7faf552fd63cd8bd046be1ab201e4b52d80f013f
Holly Mastromonica <holly.mastromonica@gmail.com>
2019-06-23 10:39:52 -0400

    Added one description to the config file

--- TT_EarlySeason/include/config.h
+++ TT_EarlySeason/include/config.h
@@ -12,3 +12,4 @@ vex::motor drive_motor_1 (vex::PORT1);
 vex::motor drive_motor_2 (vex::PORT2);
 vex::motor drive_motor_3 (vex::PORT3);
 vex::motor drive_motor_4 (vex::PORT4);   
+//If you press "alt" and then click the different lines you want to write in, you can write multiple things at once//
