75839a8cc1491c0e1d1e20b2fe11dfb776b0ae20
Holly <holly.mastromonica@gmail.com>
2019-06-27 14:01:48 -0400

    Added a space in Tanav's code in config file

--- TT_EarlySeason/include/config.h
+++ TT_EarlySeason/include/config.h
@@ -13,5 +13,5 @@ vex::motor drive_motor_2 (vex::PORT2);
 vex::motor drive_motor_3 (vex::PORT3);
 vex::motor drive_motor_4 (vex::PORT4);  
 
-void drive(int x, int y);
+void drive (int x, int y);
 //If you press "alt" and then click the different lines you want to write in, you can write multiple things at once//
