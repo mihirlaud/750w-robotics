7da9b43547385c9da5e574ca5186ab2ac3f5191e
Mihir Laud <mihirlaud@gmail.com>
2019-06-27 13:47:33 -0400

    Cleared config file

--- TT_EarlySeason/include/config.h
+++
@@ -1,7 +0,0 @@
-vex::brain Brain;
-vex::competition Competition;
-vex::controller Controller(vex::controllerType::primary);
-
-vex::motor flywheel(vex::PORT1);
-vex::pot potent(Brain.ThreeWirePort.A);
-vex::gyro g(Brain.ThreeWirePort.B);
