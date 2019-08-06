ef7563884ddd8a2ca0300e5c9e917cd1d3ffb6f3
Tanav <itstanav@gmail.com>
2019-08-05 21:00:07 -0400

    Code Fixing Pre-Scrims (8/5)

--- TT_EarlySeason/include/config.h
+++ TT_EarlySeason/include/config.h
@@ -9,12 +9,13 @@ vex::controller joystick (vex::controllerType::primary);
 //for controllers, you have to secify the type//  
 
 vex::motor driveFL(vex::PORT20, false);
-vex::motor driveFR(vex::PORT10, false);
+vex::motor driveFR(vex::PORT7, false);
 vex::motor driveBL(vex::PORT19, false);
 vex::motor driveBR(vex::PORT9, false);
 vex::motor rollerL(vex::PORT11, false);
-vex::motor rollerR(vex::PORT1, false);
+vex::motor rollerR(vex::PORT3, false);
 vex::motor spine(vex::PORT2, false);
 
 void drive(int x, int y);
+bool speedChange(bool newSpeed);
 //If you press "alt" and then click the different lines you want to write in, you can write multiple things at once//
