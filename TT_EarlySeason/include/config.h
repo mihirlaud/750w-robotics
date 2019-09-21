7d3348721d3fb165921735692e9f9f3ccb8032e5
Tanav <itstanav@gmail.com>
2019-09-21 16:45:11 -0400

    Finish auton, fix powershare, test constants

--- TT_EarlySeason/include/config.h
+++ TT_EarlySeason/include/config.h
@@ -10,8 +10,14 @@
 //The encoder position for the accumulator while intaking cubes
 #define INTAKING_POS 300
 
+//The encoder value for the lift to reach high towers
+#define HIGH_POS 300
 
-//---------------------------------------------------------
+//The encoder value for the lift to reach mid towers
+#define MID_POS 200
+
+//The encoder value for the lift to reach low towers
+#define LOW_POS 100
 
 vex::brain cortex;   
 //this defines the brain - called the default constructor because it does not take in any arguements// 
@@ -21,18 +27,24 @@ vex::competition Competition;
 //default constructor//
 
 vex::controller joystick (vex::controllerType::primary); 
-//for controllers, you have to secify the type//  
+//for controllers, you have to specify the type//  
 
 vex::motor driveFL(vex::PORT20, false);
-vex::motor driveFR(vex::PORT7, false);
-vex::motor driveBL(vex::PORT19, false);
-vex::motor driveBR(vex::PORT9, false);
-vex::motor rollerL(vex::PORT11, false);
-vex::motor rollerR(vex::PORT3, false);
-vex::motor spine(vex::PORT2, false);
+vex::motor driveFR(vex::PORT3, false);
+vex::motor driveBL(vex::PORT4, false);
+vex::motor driveBR(vex::PORT5, false);
+vex::motor intakeL(vex::PORT11, false);
+vex::motor intakeR(vex::PORT3, false);
+vex::motor liftL(vex::PORT2, false);
+vex::motor liftR(vex::PORT10, false);
+
+vex::pot potentiometerL(cortex.ThreeWirePort.A);
+vex::pot potentiometerR(cortex.ThreeWirePort.B);
 
 void drive(int x, int y);
-//If you press "alt" and then click the different lines you want to write in, you can write multiple things at once//
+void tower(int mode, int type);
+void stack();
+
 void drive_for(int dist, double speed);
 void cw_turn_for(int angle, double speed);
 void ccw_turn_for(int angle, double speed);
@@ -40,3 +52,4 @@ void intake_in(double speed);
 void intake_out(double speed);
 void intake_stop();
 void accumulator_to(int pos, double speed);
+void lift_reset(double speed);
--- TT_EarlySeason/src/main.cpp
+++ TT_EarlySeason/src/main.cpp
@@ -1,44 +1,16 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\mihir                                            */
-/*    Created:      Thu Jun 20 2019                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-#include "C:/Program Files (x86)/VEX Robotics/VEXcode/sdk/vexv5/gcc/include/stdio.h"
-//#include "C:/Program Files (x86)/VEX Robotics/VEXcode/sdk/vexv5/include/vex_brain.h"
-//#include "C:/Program Files (x86)/VEX Robotics/VEXcode/sdk/vexv5/include/vex_controller.h"
 #include "vex.h"
 #include "Robot.h"
 #include "config.h"
 #include "auton_selector.h"
+#include <cmath>
 
-bool driveSlow = false;
+int height = 0;
 
 using namespace vex;
 
-// A global instance of vex::brain used for printing to the V5 brain screen
-
-// A global instance of vex::competition
-
-
-// define your global instances of motors and other devices here
-
-
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the cortex has been powered on and    */ 
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
-int auton_index = 1;
+int auton_index = -1;
 void pre_auton( void ) {
+
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
   // AutonSelector selector(cortex);
@@ -50,17 +22,10 @@ void pre_auton( void ) {
   
   // auton_index = selector.getCode();
   // spine.resetRotation();
-}
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+  liftL.resetRotation();
+  liftR.resetRotation();
+}
 
 void drive_for(int dist, double speed=60) {
   driveFL.rotateFor(directionType::fwd, dist, rotationUnits::deg, speed, velocityUnits::pct, false);
@@ -83,215 +48,176 @@ void ccw_turn_for(int angle, double speed=60) {
 }
 
 void intake_in(double speed=50) {
-  rollerL.setVelocity(speed, vex::percentUnits::pct);
-  rollerR.setVelocity(-speed, vex::percentUnits::pct);
+  intakeL.setVelocity(speed, vex::percentUnits::pct);
+  intakeR.setVelocity(-speed, vex::percentUnits::pct);
   
-  rollerL.spin(directionType::fwd);
-  rollerR.spin(directionType::fwd);
+  intakeL.spin(directionType::fwd);
+  intakeR.spin(directionType::fwd);
 }
 
 void intake_out(double speed=50) {
-  rollerL.setVelocity(-speed, vex::percentUnits::pct);
-  rollerR.setVelocity(speed, vex::percentUnits::pct);
+  intakeL.setVelocity(-speed, vex::percentUnits::pct);
+  intakeR.setVelocity(speed, vex::percentUnits::pct);
   
-  rollerL.spin(directionType::fwd);
-  rollerR.spin(directionType::fwd);
+  intakeL.spin(directionType::fwd);
+  intakeR.spin(directionType::fwd);
 }
 
 void intake_stop() {  
-  rollerL.stop(brakeType::hold);
-  rollerR.stop(brakeType::hold);
+  intakeL.stop(brakeType::hold);
+  intakeR.stop(brakeType::hold);
 }
 
-
 void accumulator_to(int pos, double speed=50) {
-  spine.rotateTo(pos, rotationUnits::deg, speed, velocityUnits::pct, true);
+  driveFL.rotateFor(pos, rotationUnits::deg, speed, velocityUnits::pct, false);
+  driveFR.rotateFor(pos, rotationUnits::deg, speed, velocityUnits::pct, false);
+  driveBL.rotateFor(-pos, rotationUnits::deg, speed, velocityUnits::pct, false);
+  driveBR.rotateFor(-pos, rotationUnits::deg, speed, velocityUnits::pct, true);
+
+  task::sleep(250);
+}
+void lift_reset(double speed=50) {
+  liftL.rotateTo(0, rotationUnits::deg, speed, velocityUnits::pct, false);
+  liftR.rotateTo(0, rotationUnits::deg, speed, velocityUnits::pct, true);
+
   task::sleep(250);
 }
 
 void autonomous( void ) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  
   switch(auton_index) {
-    case -1:
-      cw_turn_for(FULL_CIRCLE);
+    case -1: //TESTING AUTON
       break;
     case 0: //RED GOAL SIDE
+
+      drive_for(0.30 * FULL_TILE, 70);
+      accumulator_to(INTAKING_POS);
+      drive_for(0.20 * FULL_TILE);
+      intake_in();
+      drive_for(1.0 * FULL_TILE, 20);
+      intake_stop();
+
       break;
     case 1: //RED TOWER SIDE
-    /*
-      //Go forward until the accumulator can be deployed
-      drive_for(0.30 * FULL_TILE, 70);
-      //Deploy accumulator
-      accumulator_to(STACKING_POS);
-      //Return accumulator to intaking position
-      accumulator_to(INTAKING_POS);
-      //Drive forward so that bot is closer to cubes
-      drive_for(0.20 * FULL_TILE);
-      //Begin intaking
-      intake_in();
-      //Drive forward to intake cubes
-      drive_for(1.0 * FULL_TILE, 20);
-      //Stop intake
-      intake_stop();
-      //Drive backwards to be closer to goal
-      drive_for(-1.0 * FULL_TILE);
-      //Turn to align with goal
-      cw_turn_for(0.36 * FULL_CIRCLE);
-      //Drive up to goal
-      drive_for(0.93 * FULL_TILE);
-      //Ready accumulator for stacking
-      accumulator_to(STACKING_POS);
-      //Stack cubes into goal
-      intake_out();
-      //Delay for a short while so that cubes can stack
-      task::sleep(800);
-      //Drive backwards so that cubes count as scored
-      drive_for(-1.0 * FULL_TILE, 20);
-      */
-
-//Tanav's scuffed auton RED
-      drive_for(.3 * FULL_TILE);
-      intake_out();
-      task::sleep(800);
-      drive_for(-1.0 * FULL_TILE, 20);
-      intake_stop();
-
       break;
     case 2: //BLUE GOAL SIDE
       break;
     case 3: //BLUE TOWER SIDE
-    /*
-      //Go forward until the accumulator can be deployed
-      drive_for(0.30 * FULL_TILE, 70);
-      //Deploy accumulator
-      accumulator_to(STACKING_POS);
-      //Return accumulator to intaking position
-      accumulator_to(INTAKING_POS);
-      //Drive forward so that bot is closer to cubes
-      drive_for(0.20 * FULL_TILE);
-      //Begin intaking
-      intake_in();
-      //Drive forward to intake cubes
-      drive_for(1.0 * FULL_TILE, 20);
-      //Stop intake
-      intake_stop();
-      //Drive backwards to be closer to goal
-      drive_for(-1.0 * FULL_TILE);
-      //Turn to align with goal
-      ccw_turn_for(0.36 * FULL_CIRCLE);
-      //Drive up to goal
-      drive_for(0.93 * FULL_TILE);
-      //Ready accumulator for stacking
-      accumulator_to(STACKING_POS);
-      //Stack cubes into goal
-      intake_out();
-      //Delay for a short while so that cubes can stack
-      task::sleep(800);
-      //Drive backwards so that cubes count as scored
-      drive_for(-1.0 * FULL_TILE, 20);
-      */
-
-//Tanav's scuffed auton BLUE
-      drive_for(.3 * FULL_TILE);
-      intake_out();
-      task::sleep(800);
-      drive_for(-1.0 * FULL_TILE, 20);
-      intake_stop();
-
-
-
-      break;
-    default:
       break;
   }
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
 void usercontrol( void ) {
-  // User control code here, inside the loop
 
   while (1) {
-
-    if(joystick.ButtonA.pressing()){
-      vex::task::sleep(100);
-      driveSlow = !driveSlow;
-    }
     
     drive( joystick.Axis3.position(), joystick.Axis1.position() );
 
-    if(joystick.ButtonR1.pressing()){
-      rollerL.setVelocity(70, vex::percentUnits::pct);
-      rollerR.setVelocity(-70, vex::percentUnits::pct);
+    if(joystick.ButtonL1.pressing()){
+      vex::task::sleep(100);
 
-      rollerL.spin(directionType::fwd);
-      rollerR.spin(directionType::fwd);
+      tower(height, 0);
+    }
+
+    if(joystick.ButtonL2.pressing()){
+      vex::task::sleep(100);
+
+      tower(height, 1);
+    }
+
+    if(joystick.ButtonR1.pressing()){
+      intakeL.setVelocity(70, vex::percentUnits::pct);
+      intakeR.setVelocity(-70, vex::percentUnits::pct);
+
+      intakeL.spin(directionType::fwd);
+      intakeR.spin(directionType::fwd);
     }
     else if(joystick.ButtonR2.pressing()){
-      rollerL.setVelocity(-70, vex::percentUnits::pct);
-      rollerR.setVelocity(70, vex::percentUnits::pct);
+      intakeL.setVelocity(-70, vex::percentUnits::pct);
+      intakeR.setVelocity(70, vex::percentUnits::pct);
 
-      rollerL.spin(directionType::fwd);
-      rollerR.spin(directionType::fwd);
+      intakeL.spin(directionType::fwd);
+      intakeR.spin(directionType::fwd);
     }
     else{
-      rollerL.stop(brakeType::hold);
-      rollerR.stop(brakeType::hold);
-    }  
-    
-    if(joystick.ButtonL1.pressing()){
-      spine.setVelocity(20, vex::percentUnits::pct);
-
-      spine.spin(directionType::fwd);
+      intakeL.stop(brakeType::hold);
+      intakeR.stop(brakeType::hold);
     }
-    else if(joystick.ButtonL2.pressing()){
-      spine.setVelocity(-20, vex::percentUnits::pct);
 
-      spine.spin(directionType::fwd);
+    if(joystick.ButtonUp.pressing()){ //DIRECTION NEEDS TESTING (Should be up to down)
+      driveFL.setVelocity(75, vex::percentUnits::pct);
+      driveFR.setVelocity(75, vex::percentUnits::pct);
+      driveBL.setVelocity(-75, vex::percentUnits::pct);
+      driveBR.setVelocity(-75, vex::percentUnits::pct);
+
+      driveFL.spin(directionType::fwd);
+      driveFR.spin(directionType::fwd);
+      driveBL.spin(directionType::fwd);
+      driveBR.spin(directionType::fwd);
+    }
+
+    if(joystick.ButtonRight.pressing()){ //DIRECTION NEEDS TESTING (Should be down to up)
+      driveFL.setVelocity(-75, vex::percentUnits::pct);
+      driveFR.setVelocity(-75, vex::percentUnits::pct);
+      driveBL.setVelocity(75, vex::percentUnits::pct);
+      driveBR.setVelocity(75, vex::percentUnits::pct);
+
+      driveFL.spin(directionType::fwd);
+      driveFR.spin(directionType::fwd);
+      driveBL.spin(directionType::fwd);
+      driveBR.spin(directionType::fwd);
+    }
+
+    if(joystick.ButtonLeft.pressing()){
+      vex::task::sleep(100);
+
+      stack();
+    }
+
+    if(joystick.ButtonX.pressing()){
+      vex::task::sleep(100);
+      height++;
+
+      if(height == 3)
+        height = 0;
+    }
+
+    if(joystick.ButtonA.pressing()){
+      vex::task::sleep(100);
+      height--;
+
+      if(height == -1)
+        height = 2;
+    }
+
+    if(joystick.ButtonY.pressing()){
+      liftL.setVelocity(-25, vex::percentUnits::pct);
+      liftR.setVelocity(-25, vex::percentUnits::pct);
+
+      liftL.spin(directionType::fwd);
+      liftR.spin(directionType::fwd);
+    }
+    else if(joystick.ButtonB.pressing()){
+      liftL.setVelocity(25, vex::percentUnits::pct);
+      liftR.setVelocity(25, vex::percentUnits::pct);
+
+      liftL.spin(directionType::fwd);
+      liftR.spin(directionType::fwd);
     }
     else{
-      spine.stop(brakeType::hold);
+      liftL.stop(brakeType::hold);
+      liftR.stop(brakeType::hold);
     }
-
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo 
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to 
-    // update your motors, etc.
-    // ........................................................................
-    //if(Controller.ButtonA.pressing()) {
-    //  Brain.Screen.print("Hello");
-    //}
-
-    //flywheel.setVelocity(Controller.Axis1.value(), vex::percentUnits::pct);
-    //if(potent.value(vex::percentUnits::pct) > 75)
-    //  flywheel.stop();
-
-    //Brain.Screen.print("%lf", g.value(vex::rotationUnits::deg));
 
     vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
   }
 }
 
-void drive(int x, int y){
- 
-  if(!driveSlow){
-    driveFL.setVelocity(y + x, vex::percentUnits::pct);
-    driveFR.setVelocity(y - x, vex::percentUnits::pct);
-    driveBL.setVelocity(y + x, vex::percentUnits::pct);
-    driveBR.setVelocity(y - x, vex::percentUnits::pct);
+void drive(int x, int y) {
+    driveFL.setVelocity(100-(sqrt(10000-(pow(y+x,2)))), vex::percentUnits::pct);
+    driveFR.setVelocity(100-(sqrt(10000-(pow(y-x,2)))), vex::percentUnits::pct);
+    driveBL.setVelocity(100-(sqrt(10000-(pow(y+x,2)))), vex::percentUnits::pct);
+    driveBR.setVelocity(100-(sqrt(10000-(pow(y-x,2)))), vex::percentUnits::pct);
 
     driveFL.spin(directionType::fwd);
     driveFR.spin(directionType::fwd);
@@ -299,24 +225,110 @@ void drive(int x, int y){
     driveBR.spin(directionType::fwd);
 }
 
-  else{
-    driveFL.setVelocity((y + x)/3, vex::percentUnits::pct);
-    driveFR.setVelocity((y - x)/3, vex::percentUnits::pct);
-    driveBL.setVelocity((y + x)/3, vex::percentUnits::pct);
-    driveBR.setVelocity((y - x)/3, vex::percentUnits::pct);
+void tower(int mode, int type) {
 
-    driveFL.spin(directionType::fwd);
-    driveFR.spin(directionType::fwd);
-    driveBL.spin(directionType::fwd);
-    driveBR.spin(directionType::fwd);
+double valL = potentiometerL.value(vex::rotationUnits::deg);
+double valR = potentiometerR.value(vex::rotationUnits::deg);
+double valAvg = (valL + valR)/2.0;
+
+  switch(mode) {
+    case 0: {
+      while( fabs( (LOW_POS - valAvg)) > .1 ){
+        valL = potentiometerL.value(vex::rotationUnits::deg);
+        valR = potentiometerR.value(vex::rotationUnits::deg);
+
+        double low_errorL = LOW_POS - valL;
+        double low_errorR = LOW_POS - valR;
+        double low_errorAvg = (low_errorL + low_errorR)/2.0;
+
+        double low_kP = .100;
+
+        liftL.setVelocity(low_errorAvg * low_kP, vex::percentUnits::pct);
+        liftR.setVelocity(low_errorAvg * low_kP, vex::percentUnits::pct);
+      }      
+        if(type == 0) {
+          intake_in();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        if(type == 1) {
+          intake_out();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        lift_reset();
+      break;
+    }
+    case 1: {
+      while( fabs( (MID_POS - valAvg)) > .1 ){
+        valL = potentiometerL.value(vex::rotationUnits::deg);
+        valR = potentiometerR.value(vex::rotationUnits::deg);
+
+        double mid_errorL = MID_POS - valL;
+        double mid_errorR = MID_POS - valR;
+        double mid_errorAvg = (mid_errorL + mid_errorR)/2.0;
+
+        double mid_kP = .100;
+
+        liftL.setVelocity(mid_errorAvg * mid_kP, vex::percentUnits::pct);
+        liftR.setVelocity(mid_errorAvg * mid_kP, vex::percentUnits::pct);
+      }
+        if(type == 0) {
+          intake_in();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        if(type == 1) {
+          intake_out();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        lift_reset();
+      break;
+    }
+    case 2: {
+      while( fabs( (HIGH_POS - valAvg)) > .1 ){
+        valL = potentiometerL.value(vex::rotationUnits::deg);
+        valR = potentiometerR.value(vex::rotationUnits::deg);
+
+        double high_errorL = HIGH_POS - valL;
+        double high_errorR = HIGH_POS - valR;
+        double high_errorAvg = (high_errorL + high_errorR)/2.0;
+
+        double high_kP = .100;
+
+        liftL.setVelocity(high_errorAvg * high_kP, vex::percentUnits::pct);
+        liftR.setVelocity(high_errorAvg * high_kP, vex::percentUnits::pct);
+      }
+        if(type == 0) {
+          intake_in();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        if(type == 1) {
+          intake_out();
+          vex::task::sleep(800);
+          intake_stop();
+        }
+        lift_reset();
+      break;
+    }
   }
+}
+
+void stack() {  
+
+  drive_for(0.93 * FULL_TILE);
+  accumulator_to(STACKING_POS);
+  intake_out(30);
+  task::sleep(800);
+  drive_for(-1.0 * FULL_TILE, 20);
+  accumulator_to(INTAKING_POS);
 
 }
 
 
-//
 // Main will set up the competition functions and callbacks.
-//
 int main() {
     //Set up callbacks for autonomous and driver control periods.
     Competition.autonomous( autonomous );
@@ -327,10 +339,12 @@ int main() {
        
     //Prevent main from exiting with an infinite loop.                        
     while(1) {
-      if(driveSlow)
-        joystick.Screen.print("Slow");
-      else
-        joystick.Screen.print("Fast");
+      if(height == 0)
+        joystick.Screen.print("Low");
+      else if(height == 1)
+        joystick.Screen.print("Mid");
+      else if(height == 2)
+        joystick.Screen.print("High");
       vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
     }    
        
